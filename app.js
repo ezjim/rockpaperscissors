a6947c579551d0c29743cd5c68165408880d7f3b
James <james.re@yahoo.com>
2020-01-16 09:39:03 -0800

    1st push, html is kinda done

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
-import {  }
-
-const 
+import { getRandomThrow, checkResult } from './get-random-throw.js';
+const resultDisplay = document.getElementById('result-display');
+const button = document.getElementById('submit-button');
--- rps.js
+++ rps.js
@@ -1,6 +1,17 @@
-function getRandomthrow() {
-    const randomNumber = math.floor(math.random() * 3);
-    return getThrowResults (randomNumber);
+ function getRandomThrow() {
+   Math.floor(Math.random() * 3);
+if (cpuChoice < 0) {
+    cpuChoice = 'ro';
+} else if(cpuChoice <= 1) {
+    cpuChoice ='sham';
+} else {
+    cpuChoice = 'bo';
 }
 
-function getThrowResults 
+}
+
+function compare(choice1,choice2) {
+  if (choice1 === choice2) {
+    return 'TIE!!'
+  }
+}

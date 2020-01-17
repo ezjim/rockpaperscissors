c056b05ee67fca74d99774a4c14e664dfa935ac2
asiamorozova <57855398+asiamorozova@users.noreply.github.com>
2020-01-17 11:51:12 -0800

    working on a lab

--- app.js
+++ app.js
@@ -1,32 +1,37 @@
 import { getRandomThrow } from './rps.js';
-import { checkResults } from './checkResults'
+import { checkResults } from './checkResults.js'
 
-const playButton = document.getElementById('selection');
+const playButton = document.getElementById('play');
 const resetButton = document.getElementById('reset');
 const totalWins = document.getElementById('total-wins');
 const totalLosses = document.getElementById('total-losses');
 const totalDraws = document.getElementById('total-draws');
 const displayPlayer = document.getElementById('you-win');
-const resultsContainer = document.getElementById('results-container');
+
+//const resultsContainer = document.getElementById('results-container');
+
 let wins = 0;
 let losses = 0;
 let draws = 0;
+
 playButton.addEventListener('click', () => {
-    const playerChoice = document.querySelector('input:checked').value;
-    const computerChoice = getRandomThrow();
-    console.log('comp choice', computerChoice);
-    console.log('player choice', playerChoice);
-    console.log('function', checkResults(computerChoice, playerChoice));
-    if (checkResults(computerChoice, playerChoice) === 'lose') {
+
+    const player = document.querySelector('input:checked').value;
+
+    const cpu = getRandomThrow();
+    console.log('cpu', player, cpu);
+    console.log('player', player, cpu);
+    console.log('function', checkResults(player, cpu));
+    if (checkResults(player, cpu) === 'lose') {
         losses++;
         resultsContainer.classList.remove('hidden');
         displayPlayer.textContent = 'You lost';
         totalLosses.textContent = losses;
-    } else if (checkResults(computerChoice, playerChoice) === 'win') {
+    } else if (checkResults(player, cpu) === 'win') {
         wins++;
         displayPlayer.textContent = 'You win';
         totalWins.textContent = wins;
-    } else if (checkResults(computerChoice, playerChoice) === 'draw') {
+    } else if (checkResults(player, cpu) === 'draw') {
         draws++;
         displayPlayer.textContent = "It's a Draw!";
         totalDraws.textContent = draws;
